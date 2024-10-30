947b2f0874271078728efa43bcf3dd355234a3cb
Talha shiekh <tk.shiekh4567@gmail.com>
2024-10-30 05:26:49 -0700

    Pending QR Code Scanner Screen

--- Screens/QRCodeScannerScreen.jsx
+++ Screens/QRCodeScannerScreen.jsx
@@ -1,16 +1,28 @@
-import { Text , View , StyleSheet } from "react-native";
-import React from "react";
+import {Text, View, StyleSheet} from 'react-native';
+import React from 'react';
+import {RNCamera} from 'react-native-camera';
 
 export default QRCodeScannerScreen = () => {
-    return <View style={styles.container}>
-        <Text>QRCodeScannerScreen</Text>
+  return (
+    <View style={styles.container}>
+      <RNCamera
+        captureAudio={false}
+        style={{flex: 1}}
+        type={RNCamera.Constants.Type.back}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+      />
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:"red"
-    }
-})
-
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+});
